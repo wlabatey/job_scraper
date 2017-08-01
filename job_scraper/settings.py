5b442b752ce56060d6a3f094f839c2a0d528d45d
wlabatey <wlab@startmail.com>
2017-08-01 23:52:44 +0100

    Fixed css to stop janky checkbox repositioning

--- job_scraper/settings.py
+++ job_scraper/settings.py
@@ -25,12 +25,12 @@ FEED_URI = 'file:///home/will/Programming/Git/my_repos/job_scraper/json/results.
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 8
+CONCURRENT_REQUESTS = 12
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.02
+DOWNLOAD_DELAY = 0.01
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -75,14 +75,14 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = False
 # The initial download delay
 AUTOTHROTTLE_START_DELAY = 0.05
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 0.2
+AUTOTHROTTLE_MAX_DELAY = 0.1
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 4.0
+AUTOTHROTTLE_TARGET_CONCURRElCY = 12.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
