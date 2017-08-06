bd61d26854eff83f1ec654c9ae102487fd44b977
wlabatey <wlab@startmail.com>
2017-08-06 19:35:43 +0100

    Modified scrape file for crochet

--- scrape.py
+++ scrape.py
@@ -1,30 +1,15 @@
 #/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os.path
-import subprocess
+import scrapy
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from job_scraper.spiders.stackoverflow import StackOverflowSpider
+from job_scraper.spiders.dicer import DiceSpider
+from crochet import setup
+setup()
 
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-so = os.path.abspath('json/stackoverflow.json')
-dice = os.path.abspath('json/dice.json')
-spiders = {'stackoverflow': so, 'dice': dice}
-s3_bucket = 's3://jobs-json'
-static_page = os.path.abspath('static_page/index.html')
-
-process = CrawlerProcess(get_project_settings())
-
-for k in spiders.keys():
-    print("Added spider: " + k)
-    process.crawl(k)
-process.start()
-print("Finished scraping...")
-
-print("Done. Check DynamoDB!")
-# print("Uploading to S3...")
-# for v in spiders.values():
-    # subprocess.Popen(['/usr/bin/s3cmd', 'put', v, '--acl-public', s3_bucket])
-
-# print("Opening browser...")
-# subprocess.Popen(['google-chrome', static_page])
+runner = CrawlerRunner()
+runner.crawl(StackOverflowSpider)
+runner.crawl(DiceSpider)
+d = runner.join()
