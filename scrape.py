b8eee6a114329f0b8b9675b16bf3a3d0221354cd
wlabatey <wlab@startmail.com>
2017-08-11 12:28:47 +0100

    Checked out old scrape.py file to run from cli

--- scrape.py
+++ scrape.py
@@ -1,16 +1,30 @@
 #/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import scrapydo
-import logging
-from job_scraper.spiders.stackoverflow import StackOverflowSpider
-from job_scraper.spiders.dice import DiceSpider
+import os.path
+import subprocess
 
-os.environ['SCRAPY_SETTINGS_MODULE'] = 'job_scraper.settings'
-logging.root.setLevel(logging.INFO)
-scrapydo.setup()
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
-def start_scrape(event, context):
-    scrapydo.run_spider(StackOverflowSpider)
-    scrapydo.run_spider(DiceSpider)
+so = os.path.abspath('json/stackoverflow.json')
+dice = os.path.abspath('json/dice.json')
+spiders = {'stackoverflow': so, 'dice': dice}
+s3_bucket = 's3://jobs-json'
+static_page = os.path.abspath('static_page/index.html')
+
+process = CrawlerProcess(get_project_settings())
+
+for k in spiders.keys():
+    print("Added spider: " + k)
+    process.crawl(k)
+process.start()
+print("Finished scraping...")
+
+print("Done. Check DynamoDB!")
+# print("Uploading to S3...")
+# for v in spiders.values():
+    # subprocess.Popen(['/usr/bin/s3cmd', 'put', v, '--acl-public', s3_bucket])
+
+# print("Opening browser...")
+# subprocess.Popen(['google-chrome', static_page])
