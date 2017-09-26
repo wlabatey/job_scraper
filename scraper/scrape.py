c33dada3d47312d273796a6d351bf16887a078e6
Sam Hartley <samhartley92@gmail.com>
2017-09-26 14:51:04 +0100

    Removed old subprocess comments

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -9,14 +9,7 @@ import subprocess
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-# so = os.path.abspath('json/stackoverflow.json')
-# dice = os.path.abspath('json/dice.json')
-# spiders = {'stackoverflow': so, 'dice': dice}
-# s3_bucket = 's3://jobs-json'
-# static_page = os.path.abspath('../site/index.html')
-
 spiders = ['stackoverflow']
-
 process = CrawlerProcess(get_project_settings())
 
 for spider in spiders:
@@ -30,9 +23,4 @@ jobs = []
 with open('./json/jobs.json') as job_file:
     jobs = json.load(job_file)
 
-# print("Uploading to S3...")
-# for v in spiders.values():
-    # subprocess.Popen(['/usr/bin/s3cmd', 'put', v, '--acl-public', s3_bucket])
-
-# print("Opening browser...")
-# subprocess.Popen(['google-chrome', static_page])
+pprint(jobs)
